c01e049a160accb6afc6088d4966d7308fa7f7fb
feeltr <shadowhorus@hotmail.com>
2015-10-19 00:14:01 -0500

    se agregan los atributos necesarios y el constructor así como un getter para el nombre

--- Tiendaweb/src/java/Tienda/Producto.java
+++ Tiendaweb/src/java/Tienda/Producto.java
@@ -1,3 +1,14 @@
 public class Producto{
-	
+	String nombre;
+	String descripcion;
+	Float precio;
+
+	public String getNombre(){
+		return nombre;
+	}
+	public Producto(String nombre, String descripcion, Float precio){
+		this.nombre=nombre;
+		this.descripcion=descripcion;
+		this.precio=precio;
+	}
 }
